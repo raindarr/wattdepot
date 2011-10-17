1c1d1deeb1ca0672a99c2753b5f87aced6505eaf
rbrewer <rbrewer@962b3a1e-5fa1-11de-82f7-67359cf3946f>
2011-10-17 04:25:13 +0000

    One-off hack to delete some bad SensorData
    git-svn-id: https://wattdepot.googlecode.com/svn/trunk@341 962b3a1e-5fa1-11de-82f7-67359cf3946f

---
+++ src/org/wattdepot/tinker/DeleteSensorData.java
@@ -0,0 +1,45 @@
+package org.wattdepot.tinker;
+
+import java.util.List;
+import javax.xml.datatype.XMLGregorianCalendar;
+import org.wattdepot.client.WattDepotClient;
+import org.wattdepot.resource.sensordata.jaxb.SensorDataIndex;
+import org.wattdepot.resource.sensordata.jaxb.SensorDataRef;
+import org.wattdepot.resource.source.summary.jaxb.SourceSummary;
+import org.wattdepot.util.tstamp.Tstamp;
+
+public class DeleteSensorData {
+
+  public static final String hostUri = "http://server.wattdepot.org:8190/wattdepot/";
+  public static String username;
+  public static String password;
+  public static String source;
+
+  /**
+   * @param args
+   * @throws Exception If things go wrong
+   */
+  public static void main(String[] args) throws Exception {
+    username = args[0];
+    password = args[1];
+    source = args[2];
+    WattDepotClient client = new WattDepotClient(hostUri, username, password);
+
+    SourceSummary summary = client.getSourceSummary(source);
+    System.out.println(summary);
+
+    XMLGregorianCalendar startDate = Tstamp.makeTimestamp("2011-10-12T16:08:45.543-10:00");
+    XMLGregorianCalendar endDate = Tstamp.makeTimestamp("2011-10-14T14:19:14.040-10:00");
+
+    SensorDataIndex index = client.getSensorDataIndex(source, startDate, endDate);
+    List<SensorDataRef> refs = index.getSensorDataRef();
+    System.out.println("SensorData resources from " + startDate + " to " + endDate + ": "
+        + refs.size());
+
+    for (SensorDataRef ref : refs) {
+      client.deleteSensorData(source, ref.getTimestamp());
+    }
+    summary = client.getSourceSummary(source);
+    System.out.println(summary);
+  }
+}
