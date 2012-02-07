b45c5c4d8feab3f45220df12ea595037597720ad
Robert Brewer <rbrewer@lava.net>
2012-02-06 14:57:43 -1000

    Removed race condition in TestDerbyStorageImplementation (hopefully)
    TestDerbyStorageImplementation would fail occasionally, but without nay
    clear pattern. The snapshot is made asynchronously, so it is possible
    that the JUnit assert happened before the snapshot had been made. This
    change now tests the backup log file (which should be written to
    faster), and also puts a 2 second sleep as a kludge to reduce the
    chance of a race condition. Hopefully this fixes things.

--- src/org/wattdepot/server/db/derby/TestDerbyStorageImplementation.java
+++ src/org/wattdepot/server/db/derby/TestDerbyStorageImplementation.java
@@ -8,36 +8,46 @@ import org.wattdepot.server.ServerProperties;
 import org.wattdepot.server.db.DbManagerTestHelper;
 
 /**
- * Tests functionality that is specific to the DerbyStorageImplementation, that cannot be assumed
- * at the DbManager level.
+ * Tests functionality that is specific to the DerbyStorageImplementation, that cannot be assumed at
+ * the DbManager level.
  * 
  * @author Robert Brewer
  */
 public class TestDerbyStorageImplementation extends DbManagerTestHelper {
   /**
-   * Tests the makeSnapshot method. Checks to see if the modification date of the snapshot dir is
+   * Tests the makeSnapshot method. Checks to see if the modification date of the backup log is
    * updated by the method as a quick check that something happened.
+   * 
+   * @throws InterruptedException If sleep kludge is interrupted.
    */
   @Test
-  public void testMakeSnapshot() {
+  public void testMakeSnapshot() throws InterruptedException {
     File snapshotDir = new File(server.getServerProperties().get(ServerProperties.DB_SNAPSHOT_KEY));
+    File backupLog =
+        new File(server.getServerProperties().get(ServerProperties.DB_DIR_KEY)
+            + "/wattdepot/BACKUP.HISTORY");
     Date before = null;
-    if (snapshotDir.exists()) {
-      assertTrue("Snapshot path isn't a directory!", snapshotDir.isDirectory());
-      before = new Date(snapshotDir.lastModified());
+
+    if (backupLog.exists()) {
+      assertTrue("Backup logfile isn't a file!", backupLog.isFile());
+      before = new Date(backupLog.lastModified());
       // System.out.format("before: %1$ta %1$tb %1$td %1$tT.%1$tL %n", before); // DEBUG
     }
     else {
       System.out.println("before: [doesn't exist]");
     }
     assertTrue("Unable to create snapshot", manager.makeSnapshot());
+    // There is a potential race condition since we might test for the snapshot before it is made
+    // as the snapshot is made asynchronously. Hopefully this kludge is good enough for the test.
+    Thread.sleep(2000);
     assertTrue("No snapshot directory created", snapshotDir.isDirectory());
+    assertTrue("No backup log created", backupLog.isFile());
+
     if (before != null) {
       // Only need to check if modification time changed if there was a snapshot directory before
-      Date after = new Date(snapshotDir.lastModified());
+      Date after = new Date(backupLog.lastModified());
       // System.out.format("after: %1$ta %1$tb %1$td %1$tT.%1$tL%n", after); // DEBUG
-      assertTrue("Snapshot directory not modified", after.after(before));
+      assertTrue("Backup log not modified", after.after(before));
     }
   }
-
 }
