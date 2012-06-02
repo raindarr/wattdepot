eb0d64281d3e315652bc86ef03b3b68f568be304
Robert Brewer <rbrewer@lava.net>
2012-06-01 14:49:24 -1000

    Added new jar for PropertyAdder, renamed BridgeClient jar file

--- src/main/java/org/wattdepot/client/property/PropertyAdder.java
+++ src/main/java/org/wattdepot/client/property/PropertyAdder.java
@@ -15,17 +15,15 @@ import org.wattdepot.client.BadXmlException;
 import org.wattdepot.client.MiscClientException;
 import org.wattdepot.client.NotAuthorizedException;
 import org.wattdepot.client.OverwriteAttemptedException;
-import org.wattdepot.client.ResourceNotFoundException;
 import org.wattdepot.client.WattDepotClient;
 import org.wattdepot.datainput.DataInputClientProperties;
+import org.wattdepot.resource.property.jaxb.Properties;
 import org.wattdepot.resource.property.jaxb.Property;
-import org.wattdepot.resource.sensordata.jaxb.SensorData;
 import org.wattdepot.resource.source.jaxb.Source;
 import org.wattdepot.util.logger.WattDepotUserHome;
 
 /**
- * Adds the supportsEnergyCounters property to any Source that contains sensor data that contains
- * energy counter properties.
+ * Adds the arbitrary properties to Sources in batch mode.
  * 
  * @author Robert Brewer
  */
@@ -46,6 +44,15 @@ public class PropertyAdder {
   /** If true, just print out what would happen without making changes to Sources. */
   private boolean dryRun;
 
+  /** The Property to be added to Sources. */
+  private Property newProperty;
+
+  /** If true, skip virtual Sources when adding properties. */
+  private boolean skipVirtual;
+
+  /** If true, overwrite any existing Property with given key by new value. */
+  private boolean overwrite;
+
   /**
    * Creates the new PropertyAdder client.
    * 
@@ -53,9 +60,13 @@ public class PropertyAdder {
    * ~/.wattdepot/client/datainput.properties
    * @param uri The URI of the WattDepot server.
    * @param dryRun If true, just print out what would happen without making changes to Sources.
+   * @param newProperty The new Property to be added to Sources.
+   * @param skipVirtual If true, skip virtual Sources when adding properties.
+   * @param overwrite If true, overwrite any existing Property with given key by new value.
    * @throws IOException If the property file cannot be found.
    */
-  public PropertyAdder(String propertyFilename, String uri, boolean dryRun) throws IOException {
+  public PropertyAdder(String propertyFilename, String uri, boolean dryRun, Property newProperty,
+      boolean skipVirtual, boolean overwrite) throws IOException {
     if (propertyFilename == null) {
       // Use default property file name
       this.properties =
@@ -68,6 +79,9 @@ public class PropertyAdder {
 
     this.uri = uri;
     this.dryRun = dryRun;
+    this.newProperty = newProperty;
+    this.skipVirtual = skipVirtual;
+    this.overwrite = overwrite;
   }
 
   /**
@@ -106,98 +120,92 @@ public class PropertyAdder {
         return false;
       }
 
-      SensorData data;
       // Iterate through all sources
       for (Source source : sources) {
-        if (source.isVirtual()) {
+        if (source.isVirtual() && this.skipVirtual) {
           // we only care about non-virtual sources, so skip
           System.out.format("Skipping virtual source %s%n", source.getName());
           continue;
         }
-        String counterSupport = source.getProperty(Source.SUPPORTS_ENERGY_COUNTERS);
-        if (counterSupport == null) {
-          // counter support unknown, so see if latest sensor data contains counters
+        String existingPropertyValue = source.getProperty(this.newProperty.getKey());
+
+        if (dryRun) {
+          if (existingPropertyValue == null) {
+            System.out.format("dryRun: would have added new Property to source %s%n",
+                source.getName());
+          }
+          else if (overwrite) {
+            System.out.format("dryRun: would have overwritten new Property to source %s%n",
+                source.getName());
+          }
+          else {
+            System.out.format("dryRun: Source %s already has new Property set to \"%s\" and "
+                + "overwrite not selected, skipping%n", source.getName(), existingPropertyValue);
+          }
+        }
+        else {
+          if (existingPropertyValue == null) {
+            // new Property isn't already present, so let's add it
+            source.addProperty(newProperty);
+          }
+          else if (overwrite) {
+            // new Property is already present, but we are going to overwrite it
+            Properties properties = source.getProperties();
+            List<Property> propList = properties.getProperty();
+
+            for (int i = 0; i < propList.size(); i++) {
+              if (propList.get(i).getKey().equals(newProperty.getKey())) {
+                propList.remove(i);
+                break;
+              }
+            }
+            source.addProperty(newProperty);
+          }
+          else {
+            // Source already has new Property, and we are not overwriting so skip it
+            System.out.format("Source %s already has new Property set to \"%s\", skipping%n",
+                source.getName(), existingPropertyValue);
+            continue;
+          }
+
           try {
-            data = client.getLatestSensorData(source.getName());
+            if (!client.storeSource(source, true)) {
+              System.err.format(
+                  "Had problems updating Source %s on server, which is weird. Aborting.",
+                  source.getName());
+              return false;
+            }
+            if (existingPropertyValue == null) {
+              System.out.format("Added new Property to source %s%n", source.getName());
+            }
+            else {
+              System.out.format("Source %s already has new Property set to \"%s\", overwrote "
+                  + "with new value%n", source.getName(), existingPropertyValue);
+            }
           }
           catch (NotAuthorizedException e) {
-            // Any lack of authorization should have been caught when fetching source, but whatever
-            System.err.format("Given credentials do not allow sensor data retrieval. Aborting.%n");
-            return false;
-          }
-          catch (ResourceNotFoundException e) {
-            System.err.format("Source %s does not have any sensor data, skipping.%n", source
-                .getName());
-            continue;
+            System.err.format(
+                "Had problems updating Source %s on server, which is weird. Aborting.",
+                source.getName());
           }
           catch (BadXmlException e) {
             System.err.println("Received bad XML from server, which is weird. Aborting.");
             return false;
           }
-          catch (MiscClientException e) {
+          catch (OverwriteAttemptedException e) {
             System.err
-                .println("Had problems retrieving sensor data from server, which is weird. Aborting.");
+                .println("Attempted to overwrite source without overwrite flag (should never "
+                    + "happen). Aborting.");
             return false;
           }
-                    
-          if ((data.getProperty(SensorData.ENERGY_CONSUMED_TO_DATE) == null)
-              && (data.getProperty(SensorData.ENERGY_GENERATED_TO_DATE) == null)) {
-            System.out.format(
-                "Latest sensor data from source %s doesn't have counters, skipping %n", source
-                    .getName());
+          catch (MiscClientException e) {
+            System.err.println("Had problems storing Source on server, which is weird. Aborting.");
+            return false;
           }
-          else {
-            // Found counter(s) in latest sensor data, so add source property
-            if (dryRun) {
-              System.out.format(
-                  "dryRun: would have added energy counter properties to source %s%n", source
-                      .getName());
-            }
-            else {
-              source.addProperty(new Property(Source.SUPPORTS_ENERGY_COUNTERS, "true"));
-              try {
-                if (!client.storeSource(source, true)) {
-                  System.err.format(
-                      "Had problems updating Source %s on server, which is weird. Aborting.",
-                      source.getName());
-                  return false;
-                }
-                System.out.format("Added energy counter properties to source %s%n", source
-                    .getName());
-              }
-              catch (NotAuthorizedException e) {
-                System.err.format(
-                    "Had problems updating Source %s on server, which is weird. Aborting.", source
-                        .getName());
-              }
-              catch (BadXmlException e) {
-                System.err.println("Received bad XML from server, which is weird. Aborting.");
-                return false;
-              }
-              catch (OverwriteAttemptedException e) {
-                System.err
-                    .println("Attempted to overwrite source without overwrite flag (should never "
-                        + "happen). Aborting.");
-                return false;
-              }
-              catch (MiscClientException e) {
-                System.err
-                    .println("Had problems storing Source on server, which is weird. Aborting.");
-                return false;
-              }
-              catch (JAXBException e) {
-                System.err.println("Had some problem marshalling XML , which is weird. Aborting.");
-                return false;
-              }
-            }
+          catch (JAXBException e) {
+            System.err.println("Had some problem marshalling XML , which is weird. Aborting.");
+            return false;
           }
-        }
-        else {
-          // Source already explicitly specifies value for counter support, so skip it
-          System.out.format("Source %s already has counter support set to \"%s\", skipping%n",
-              source.getName(), counterSupport);
-
-          continue;
         }
       }
       return true;
@@ -221,10 +229,15 @@ public class PropertyAdder {
     options.addOption("p", "propertyFilename", true, "Filename of property file");
     options
         .addOption("n", "dryRun", false, "Just print what would happen without changing Sources");
+    options.addOption("a", "addProperty", true,
+        "Property and value to add, ex. \"fooProperty:true\"");
+    options.addOption("v", "skipVirtual", false, "Do not add property to virtual Sources");
+    options.addOption("o", "overwrite", false, "Overwrite any existing property with new value");
 
     CommandLine cmd = null;
-    String uri = null, propertyFilename = null;
-    boolean dryRun;
+    String uri = null, propertyFilename = null, propertyString = null;
+    Property newProperty = null;
+    boolean dryRun, skipVirtual, overwrite;
 
     CommandLineParser parser = new PosixParser();
     HelpFormatter formatter = new HelpFormatter();
@@ -253,18 +266,55 @@ public class PropertyAdder {
     if (cmd.hasOption("p")) {
       propertyFilename = cmd.getOptionValue("p");
     }
+
     dryRun = cmd.hasOption("n");
+
+    if (cmd.hasOption("a")) {
+      propertyString = cmd.getOptionValue("a");
+      String propertyName = null, propertyValue = null;
+      String[] extractedStrings = propertyString.split(":");
+      if (extractedStrings.length == 2) {
+        propertyName = extractedStrings[0].trim();
+        propertyValue = extractedStrings[1].trim();
+        if ((propertyName.length() != 0) || (propertyValue.length() != 0)) {
+          newProperty = new Property(propertyName, propertyValue);
+        }
+        else {
+          System.err.println("Supplied Property value invalid, exiting.");
+          formatter.printHelp(toolName, options);
+          System.exit(1);
+        }
+      }
+      else {
+        System.err.println("Supplied Property value invalid, exiting.");
+        formatter.printHelp(toolName, options);
+        System.exit(1);
+      }
+    }
+    else {
+      System.err.println("Required property parameter not provided, exiting.");
+      formatter.printHelp(toolName, options);
+      System.exit(1);
+    }
+
+    skipVirtual = cmd.hasOption("s");
+
+    overwrite = cmd.hasOption("o");
 
     // Results of command line processing, should probably be commented out
     System.out.println("uri: " + uri);
     System.out.println("propertyFilename: " + propertyFilename);
     System.out.println("dryRun: " + dryRun);
+    System.out.println("newProperty: " + newProperty);
+    System.out.println("skipVirtual: " + skipVirtual);
+    System.out.println("overwrite: " + overwrite);
     System.out.println();
 
     // Actually create the input client
     PropertyAdder propertyAdder = null;
     try {
-      propertyAdder = new PropertyAdder(propertyFilename, uri, dryRun);
+      propertyAdder =
+          new PropertyAdder(propertyFilename, uri, dryRun, newProperty, skipVirtual, overwrite);
     }
     catch (IOException e) {
       System.err.println("Unable to read properties file, terminating.");
